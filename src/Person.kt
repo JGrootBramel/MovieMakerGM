01dd029d5a1829ddf170a32e20aede527e62094a
JGrootBramel <joostgrootbramel@gmail.com>
2019-07-07 23:22:13 +0200

    replaced beforzugterSchauspieler und genre mit Random funktionalität added Spieldaten complete

--- src/Person.kt
+++ src/Person.kt
@@ -5,9 +5,9 @@ open class Person (val vorname : String, val name : String ) {
     var gehalt = Random.nextInt(500000 , 2000000 )
     var erfahrung = Random.nextInt(100 , 200 )
 
-    override open fun toString(): String = "Mein Name ist $vorname $name"
+    override fun toString(): String = "Mein Name ist $vorname $name"
 
-    open fun filmFertigProduziert () : Unit {
+    open fun filmFertigProduziert (){
         erfahrung += Random.nextInt( 10, 20 )
     }
 }
@@ -15,8 +15,8 @@ open class Person (val vorname : String, val name : String ) {
 class Regisseur ( vorname : String , name : String ) : Person (vorname , name) {
 
     val effizienzFaktor = Random.nextDouble( 0.7, 1.5)
-    val beforzugterSchauspieler = Schauspieler ( "" , "" )
-   
+    val beforzugterSchauspieler = Spieldaten.Spieldaten.zufaelligerSchauspieler()
+
     override fun toString(): String = "Ich bin ein Regisseur " + super.toString()
 
     fun budgetVerwertung ( budget : Int ) = (budget * effizienzFaktor).toInt()
@@ -24,7 +24,7 @@ class Regisseur ( vorname : String , name : String ) : Person (vorname , name) {
 
 class Schauspieler ( vorname: String , name : String) {
 
-    val genres = listOf<String> ( "Horror", "Comedy", "Action")
+    val genres = listOf<String>( Spieldaten.Spieldaten.zufaelligesGenre(), Spieldaten.Spieldaten.zufaelligesGenre())
 
     override fun toString(): String {
         return "Ich bin ein Schauspieler! " + super.toString()
