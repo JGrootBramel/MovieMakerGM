de501c70dc7cb5be515d12804e76df2680b15e16
JGrootBramel <joostgrootbramel@gmail.com>
2019-07-12 10:47:05 +0200

    Refactor object o to obj

---
+++ src/our_list/SimpleListForAnything.kt
@@ -0,0 +1,12 @@
+package our_list
+
+interface SimpleListForAnything {
+
+    fun addFirst ( obj : Any )
+    fun addLast ( obj : Any )
+    fun contains ( obj : Any ) : Boolean
+    fun size () : Int
+    fun getFirst () : Any
+    fun get ( n : Int ) : Any
+    fun removeObject ( obj : Any)
+}
