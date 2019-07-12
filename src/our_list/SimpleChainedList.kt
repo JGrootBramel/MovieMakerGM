e7f70475680ee7699708c26a1c90835f3d6b5a56
JGrootBramel <joostgrootbramel@gmail.com>
2019-07-12 10:58:30 +0200

    SimpleLinkedList: Added addFirst addLast

--- src/our_list/SimpleChainedList.kt
+++
@@ -1,2 +0,0 @@
-package our_list
-
---
+++ src/our_list/SimpleLinkedList.kt
@@ -0,0 +1,49 @@
+package our_list
+
+class SimpleLinkedList : SimpleListForAnything {
+
+
+    class Node ( val data : Any , var next : Node? )
+
+    private var first : Node? = null
+
+    override fun addFirst(obj: Any) {
+        first = Node(obj, first)
+    }
+
+    override fun addLast(obj: Any) {
+
+        if ( first == null ) addFirst(obj)
+
+        var runPointer = first
+
+        while (runPointer?.next != null ) {
+            runPointer = runPointer.next
+        }
+
+        runPointer?.next = Node (obj , null)
+
+
+    }
+
+    override fun contains(obj: Any): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun size(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getFirst(): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun get(n: Int): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun removeObject(obj: Any) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
